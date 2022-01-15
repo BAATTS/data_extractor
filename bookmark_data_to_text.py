53af72eaaa2e721213b2204a62720018107f04af
BAATTS <baattts@gmail.com>
2022-01-14 16:13:57 -0800

    Complete

--- bookmark_data_to_text.py
+++ bookmark_data_to_text.py
@@ -1,25 +1,54 @@
-import docx
 import os
+import zipfile
+import re
+import json
+import docx
 from docx.oxml.shared import qn
-from docx import Document
+from lxml import etree
 
-base_path = 'quote example.docx'
-
-document = docx.Document(base_path)
-doc_element = document.part._element
-bookmark_list = doc_element.xpath(".//w:bookmarkStart")
-bookmark_text = []
-for bookmark in bookmark_list:
-    par = bookmark.getparent()
-    runs = par.findall(qn('w:r'))
-    for run in runs:
-        try:
-            print(' ', run.find(qn('w:t')).text, end=' ')
-            bookmark_text.append(run.find(qn('w:t')).text)
-            with open('bookmarks_data.txt', 'w') as file:
-                file.write(str(bookmark_text) + '\n')
-        except:
-            pass
-            print('\n', '-' * 10)
+dictionary = {}
+bookmark_data = []
+base_path = 'files'
+saved_data = 'data'
+complete_saved_data = os.path.join(saved_data, 'bookmarks_data.txt')
+regex_for_price = '<w:t>(\\$[0-9]+,[0-9]+)</w:t>'
 
 
+def bookmark_data_to_text():
+    for files in os.listdir(base_path):
+        if files.endswith('.docx'):
+            document = docx.Document(files)
+            doc_element = document.part._element
+            bookmark_list = doc_element.xpath(".//w:bookmarkStart")
+
+            documentZip = zipfile.ZipFile(files)
+            xml_content = documentZip.read('word/document.xml')
+            documentZip.close()
+
+            xml_str_object = xml_content
+            root = etree.fromstring(xml_str_object)
+            xml_string = str(xml_str_object)
+
+            dollar_amounts = re.findall(regex_for_price, xml_string)
+
+            for bookmark in bookmark_list:
+                name = bookmark.get(qn('w:name'))
+                par = bookmark.getparent()
+                runs = par.findall(qn('w:r'))
+
+                bookmark_data = []
+                dictionary[name] = bookmark_data
+
+                for run in runs:
+                    try:
+                        dictionary[name].append(run.find(qn('w:t')).text)
+                        json_object = json.dumps(bookmark_data, indent=4)
+                        with open(complete_saved_data, 'w') as file:
+                            file.write(str(json_object) + '\n')
+                    except Exception as e:
+                        pass
+
+    dictionary['INVESTMENT'].append(dollar_amounts)
+    json_object = json.dumps(dictionary, indent=4)
+    with open(complete_saved_data, 'w') as file:
+        file.write(str(json_object) + '\n')
--- doc_to_docx.py
+++ doc_to_docx.py
@@ -3,32 +3,28 @@ import re
 import os
 import win32com.client as win32
 from win32com.client import constants
+from bookmark_data_to_text import bookmark_data_to_text
 
 
 # Create list of paths to .doc files
-paths = glob('C:\\Users\\JORGE\\Documents\\word_files\\*.doc', recursive=True)
-word = 'quote example.docx'
-
-
-# Convert old .doc files to .docx
-def save_as_docx(path):
-    # Opening MS Word
-    word = win32.gencache.EnsureDispatch('Word.Application')
-    doc = word.Documents.Open(path)
-
-    doc.Activate()
-
-    # Rename path with .docx
-    new_file_abs = os.path.abspath(path)
-    new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
-
-    # Save and Close
-    word.ActiveDocument.SaveAs(
-        new_file_abs, FileFormat=constants.wdFormatXMLDocument
-    )
-    doc.Close(False)
-
+paths = glob('I:\\PycharmProjects\\data_extractor\\files\\*.doc', recursive=True)
 
 for files in paths:
     if files.endswith('.doc'):
-        save_as_docx(files)
+        # Opening MS Word
+        word = win32.gencache.EnsureDispatch('Word.Application')
+        doc = word.Documents.Open(files)
+
+        doc.Activate()
+
+        # Rename path with .docx
+        new_file_abs = os.path.abspath(files)
+        new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
+
+        # Save and Close
+        word.ActiveDocument.SaveAs(
+            new_file_abs, FileFormat=constants.wdFormatXMLDocument
+        )
+        doc.Close(False)
+
+        bookmark_data_to_text()
